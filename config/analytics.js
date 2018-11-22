84ecbb316aa1780fa630e5820e8e07e5fb1ab701
Jonas Bjerg <40546928+JonasIsLive@users.noreply.github.com>
2018-11-22 12:13:30 -0800

    Update analytics.js

--- config/analytics.js
+++ config/analytics.js
@@ -2,7 +2,7 @@ import {
   GoogleAnalyticsTracker,
 } from 'react-native-google-analytics-bridge';
 
-let google_analytics_id = 'UA-100868948-1';
+let google_analytics_id = 'UA-XXXXX-1';
 
 let tracker = new GoogleAnalyticsTracker(google_analytics_id);
 
